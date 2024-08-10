5554e3e4126600110f09aa25563c6414cc813a7c
Jason Stewart <jason.stewart@gmail.com>
2024-08-10 16:32:57 -0700

    realign buttons simplify sections

--- uscivics_streamlit.py
+++ uscivics_streamlit.py
@@ -19,10 +19,10 @@ def main():
         st.session_state['section_name'] = section_name
 
     def test_section(section_name):
+                
+        questions = test.get_section_questions(section_name)        
         
-        questions = test.get_section_questions(section_name)
-        st.html('<div>')
-        st.write(section_name)
+        st.subheader('Section Test')
         for q_idx in range(len(questions)):
             qst = questions[q_idx]
             
@@ -31,18 +31,17 @@ def main():
             )
             st.subheader('Possible Answers')
             st.text('\n'.join(qst['possible answers']))
-        st.html('</div>')
+        
         # print(section)        
         # print(dumps(questions,indent=4))
 
 
     def show_section_questions(section_name):
+        st.subheader('Section Questions')
         questions = test.get_section_questions(section_name)
         questions_df = pd.DataFrame(questions)
         questions_df.set_index('number',inplace=True)
         st.write(questions_df)
-
-
 
     def update_app():
     
@@ -58,17 +57,23 @@ def main():
         print(f"Section {section_index}: {st.session_state['section_name']}")
         section = st.selectbox('Which section would you like?', options=sections, index=section_index, key='section-selector', help='Choose section' ) # on_change=section_select_callback
 
+        # Action buttons 
+        button_col1, button_col2 = st.columns([1,1])
+
         section_action = None
         # Buttons for section actions
-        if st.button("Questions"):
-            section_action = show_section_questions
-
-        if st.button("Test me!"):
-            section_action = test_section
+        with button_col1:
+            if st.button("Questions"):
+                section_action = show_section_questions
+        with button_col2:
+            if st.button("Test me!"):
+                section_action = test_section
 
         if section_action is not None:
             st.html('<div>')
-            section_action(st.session_state['section_name'])
+            section_name = st.session_state['section_name']
+            st.write(section_name)
+            section_action(section_name)
             st.html('</div>')
 
     update_app()
